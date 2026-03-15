e0afe09c015169d25f33c8246c7019253f6886dc
James Kelly <JamesKelly@mac.home>
2026-03-15 00:09:08 +0000

    remove coop headers

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-        ],
-      },
-    ]
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
